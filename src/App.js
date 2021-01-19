f8b8f8dc770ad78cc008d0164504a3eea0638e1a
123om123 <omihridesh@gmail.com>
2021-01-18 21:31:21 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage></LandingPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="titleContainer">
+      <h1 className="titleText">React Front-End Builder</h1>
+      <h4 className="titleSubtext">Type english in and get React code</h4>
+    </div>
+  );
+}
+
+export default Title;
